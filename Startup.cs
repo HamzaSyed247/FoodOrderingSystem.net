955bd38572516fcbe72757452e82a70fb0052db2
Syed Hamza Ali <132161361+HamzaSyed247@users.noreply.github.com>
2023-10-12 23:08:04 +0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,46 @@
+﻿using Flamin.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flamin
+{
+    public class Startup
+    {
+        public IConfiguration Configuration
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<ApplicationContext>(option =>
+            {
+                option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddRazorPages();
+        }
+
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
